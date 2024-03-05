e8c4941f5fe6de675513007cdfe57948eee87496
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2024-03-05 13:13:22 +0000

    Responsive screens for experience cards

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -17,12 +17,9 @@ export default function ExperienceCard({ experience }: Props) {
     //console.log(experience); // Logging experience object to console for debugging
     return (
         // Article container for the experience card
-        <article className=' border bg-[#2f2f2f] flex mx-2 flex-col items-center justify-center  flex-shrink-0 rounded-lg  space-y-1   
-        h-[420px] pt-5 m:mt-0 pb-2 mb-3 w-[320px] 
-       sm:h-full sm:w-[600px] sm:ml-[200px] sm:m-0 
-        md:w-[600px] md:h-[450px]
-
-        snap-center hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+        <article className='  bg-[#2f2f2f] flex flex-col items-center justify-center  flex-shrink-0 rounded-lg  space-y-1 
+        w-full h-full
+        snap-center snap-mandatory hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
             {/* Display company image if available */}
             {experience.companyImage && (
                 <motion.img 
@@ -33,12 +30,9 @@ export default function ExperienceCard({ experience }: Props) {
                     transition={{ duration: 1.2 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     //viewport={{ once: true }}
-                    className=' rounded-full p-2
-                     my-2
-                    h-[75px] w-[75px] 
-                    sm:h-[80px] sm:w-[80px]
-                    md:h-[120px] md:w-[120px]  
-                    xl:w-[100px] xl:h-[100px] 
+                    className=' rounded-full
+                 h-24 w-24
+                    max-w-[100px] max-h-[100px] 
                     object-cover object-center'
                     src={urlFor(experience?.companyImage).url()} // Constructing URL for the image
                     alt=''
@@ -83,7 +77,7 @@ export default function ExperienceCard({ experience }: Props) {
                 </p>
 
                 {/* Display summary points */}
-                <ul className=' list-disc space-y-2 pb-10 pr-2 text-sm h-40 md:h-40 overflow-y-scroll scrollbar scrollbar-track-[#f7ab0a]/80 scrollbar-thumb-[#f7ab0a]/80'>
+                <ul className='rounded-lg bg-[#393939] list-disc space-y-2  p-4 text-sm h-40 md:h-40 overflow-y-scroll scrollbar scrollbar-track-[#f7ab0a]/80 scrollbar-thumb-[#f7ab0a]/80'>
                     {/* Mapping over points array to display summary points */}
                     {experience.points.map((point,i) =>(
                         <li key={i}>{point}</li>
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -22,14 +22,14 @@ export default function WorkExperience({ experiences }: Props) {
       // Animation state when component is in view
       whileInView={{ opacity: 1.5, x: 0 }}
       // CSS classes for styling
-      className='h-screen flex snap-x mx-6 sm:mx-10 relative overflow-hidden  pb-12 flex-col text-left md:flex-row md:pb-16 md:pt-20 max-w-full px-1 justify-evenly  lg:mx-auto items-center space-x-1'
+      className='h-screen flex pb-32 sm:pb-20 snap-x p-5 relative overflow-hidden   flex-col text-left  max-w-full   items-center space-x-1'
     >
       {/* Heading for experience section */}
       <h3 className=' pl-3 absolute top-16 uppercase tracking-[10px] text-gray-500 text-small md:text-2xl lg:text-3xl '>
         Experience
       </h3>
       {/* Container for experience cards */}
-      <div className='border border-red-500 flex w-full justify-between sm:my-0 sm:p-6 sm:pt-24 sm:space-x-5  overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
+      <div className='pb-3 pt-24 px-8 flex w-full h-full   overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 space-x-5'>
         {/* Mapping over experiences array to render ExperienceCard for each */}
         {experiences?.map(experience => (
           <ExperienceCard key={experience._id} experience={experience} />
