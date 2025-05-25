b30d3876578aba4a7656321f0e515c91b6e2b891
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2025-05-25 10:44:01 +0100

    Remove unnecessary whitespace and add a comment in the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
   const experiences = await getExperience();
   const skills = await getSkill();
   const projects = await getProjects();
-
+//blah blah blah
   // Rendering the home page components
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen w-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 " >
